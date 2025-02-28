1df05ed55e6427a65e0ceb44b23b6f190309bf83
Pedro Henrique Barbosa Paes <padrohenriqueb@gmail.com>
2025-02-28 19:13:31 -0300

    Add files via upload

---
+++ aula.4 (teste 1).py
@@ -0,0 +1,53 @@
+# verificar qual o tipo do dado
+# type ()
+
+# para mostrar algo em ambiente de teste
+# função - ação
+
+
+#input vai inserir um dado
+nome = input ('digite seu nome')
+print ('Olá,', nome)
+
+idade = input ('digite sua idade')
+print (idade, 'anos')               
+
+#dado do tipo texto
+print ('seja bem vindo', nome)
+# dados do tipo inteiro
+#print (type(10))
+# dado do tipo real
+#print (type(10.5))
+#dado do tipo boleano
+#print (type(True))
+
+numero = float (input ('digite um número'))
+numero2 = float (input('digite outro para a multiplicação'))
+print ('O resultado da sua conta', nome, 'é:', numero*numero2)
+
+print ("agora vamos para a divisão")
+numero3 = float (input ('digite um numero'))
+numero4 = float (input ('digite  outro'))
+print ('o resultado da sua conta de divisão é:', numero3/numero4)
+
+print ('boa, agora vamos para a soma')
+numero5 = float (input ('digite um numero'))
+numero6 = float (input ('digite  outro'))
+print ('o resultado da sua conta de soma é:', numero5+numero6)
+
+print ('por fim, vamos para a subtração')
+numero7 = float (input ('digite um numero'))
+numero8 = float (input ('digite  outro'))
+print ('o resultado da sua conta de subtração é:', numero7-numero8)
+
+
+
+
+#print é uma funçao de saída
+#print (10+20)
+
+#n é uma várivel
+#n = 10
+#print (n)
+#print (n+30)
+
