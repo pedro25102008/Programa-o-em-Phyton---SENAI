e05458f7c53906f9de8d653646938043776079fc
Pedro Henrique Barbosa Paes <padrohenriqueb@gmail.com>
2025-03-30 13:57:58 -0300

    Add files via upload

---
+++ aula.6.py
@@ -0,0 +1,89 @@
+
+nome = input ("Olá, digite seu nome")
+print (f"aqui esta alguns cálculos, {nome}")
+resultado = 4*6
+print (f'o resultado da multiplicação de 4 por 6 é {resultado}')
+
+resultado1 = 5+7
+print (f'o resultado da soma de 5 + 7 é {resultado1}')
+
+resposta = input ("está entendendo?")
+if resposta == "sim" :
+    print (f'Perfeito! então vamos coninuar! o resultado da multiplicação de 4 e 6 é {resultado}')
+else :
+    print (f'calma que vai ficar mais fácil! o resultado da multiplicação de 4 e 6 é {resultado}')
+
+resultado2 = 3**4
+print (f'agora, uma área mais complexa, o cálculo de 3 elevado a 4 e {resultado2}')
+
+resultado3 = (8+12+15) /3
+print (f'a média dos números 8, 12 e 15 é {resultado3}')
+
+resultado4 = 17%4
+print (f'o cálculo do módulo de 17 por 4 é {resultado4}')
+
+input (f'tudo certo ate aqui, {nome}?')
+print (f'vamos dar sequência')
+
+resultado5_parte1 = 9*3
+print (f'a multiplicação de 9 por 3 é {resultado5_parte1}')
+resultado5_parte2 = resultado5_parte1**3
+print (f'agora, elevando ao quadrado fica {resultado5_parte2}')
+
+resultado6 = 81**(1/2)
+print (f'a raiz quadrada de 81 é {resultado6}')
+
+resultado7 = (20+3)*4
+print (f'agora, somando 20 com 3 e multiplicando 4 fica {resultado7}')
+
+resultado8_parte1 = (15*2)
+print (f'fazendo 15 vezes 2 da {resultado8_parte1}')
+resultado8_parte2 = resultado8_parte1-7
+print (f'subtraindo por 7 fica igual a {resultado8_parte2}')
+
+resultado9 = 5**3
+print (f'elevando 5 ao cubo da o resultado {resultado9}')
+
+resultado10 = (17+21+25) /3
+print (f'a média dos números 17, 21 e 25 é {resultado10}')
+
+resultado11 = (11*2)+4
+print (f'multiplicando 11 por 2 e adicionando 4 é {resultado11}')
+
+resposta2 = input ("conseguiu entender tudo?")
+if resposta2 == "sim" or "consegui" :
+    print (f'perfeito, {nome}! obrigado pela atenção!')
+else :
+    yes_or_no = input(f'quer revisar denovo?')
+    if yes_or_no == "nao" or "não" :
+        print("ok, muito obrigado mesmo assim!!")
+    if yes_or_no == "sim" or "claro" :
+        print(f'ok, {nome}, vamos começar denovo!')
+
+print (f'o resultado da multiplicação de 4 por 6 é {resultado}')
+
+print (f'o resultado da soma de 5 + 7 é {resultado1}')
+
+print (f'agora, uma área mais complexa, o cálculo de 3 elevado a 4 e {resultado2}')
+
+print (f'a média dos números 8, 12 e 15 é {resultado3}')
+
+print (f'o cálculo do módulo de 17 por 4 é {resultado4}')
+
+print (f'a multiplicação de 9 por 3 é {resultado5_parte1}')
+
+print (f'agora, elevando ao quadrado fica {resultado5_parte2}')
+
+print (f'a raiz quadrada de 81 é {resultado6}')
+
+print (f'agora, somando 20 com 3 e multiplicando 4 fica {resultado7}')
+
+print (f'fazendo 15 vezes 2 da {resultado8_parte1}')
+
+print (f'subtraindo por 7 fica igual a {resultado8_parte2}')
+
+print (f'elevando 5 ao cubo da o resultado {resultado9}')
+
+print (f'a média dos números 17, 21 e 25 é {resultado10}')
+
+print (f'multiplicando 11 por 2 e adicionando 4 é {resultado11}')
